c3dd3687af3b2870d770f6aba5a528a0ba670995
devonhackley <devonhackley@gmail.com>
2017-04-17 12:51:49 -0700

    just working through some stacks

---
+++ stacks/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+  function Stack() {
+  var items = [];
+
+  this.push = (element) => {
+    items.push(element);
+  };
+
+  this.pop = (element) => {
+    return items.pop();
+  };
+
+  this.peek = () => {
+    return items[items.length-1];
+  };
+
+  this.isEmpty = () => {
+    return items.length == 0;
+  };
+
+  this.size = () => {
+    return items.length;
+  };
+
+  this.clear = () => {
+    items = [];
+  };
+
+  this.print = () => {
+    console.log(items.toString());
+  };
+};
+
+var stack = new Stack();
+console.log(stack.isEmpty());
+
+stack.push(5);
+stack.push(8);
+stack.print();
+stack.push(11);
+stack.push(15);
+console.log(stack.size());
+console.log(stack.isEmpty());
