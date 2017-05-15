fe5ed35a3e8f1babd31a9addc410054b84cf0c1a
devonhackley <devonhackley@gmail.com>
2017-05-15 14:51:42 -0700

    worked thru bubble-sort implementation

--- front-end/sort/bubble-sort.js
+++ front-end/sort/bubble-sort.js
@@ -1,1 +1,22 @@
 'use strict';
+  function bubbleSort(nums){
+
+  let sorted = false;
+
+  while(!sorted){
+    sorted = true;
+    for(var i =0; i < nums.length - 1; i++){
+      if (nums[i] > nums[i + 1]) {
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        sorted = false;
+      }
+    }
+  }
+  return nums;
+}
+
+let arr = [4,5,3,6,1,2];
+
+console.log(bubbleSort(arr)); // [1,2,3,4,5,6]
