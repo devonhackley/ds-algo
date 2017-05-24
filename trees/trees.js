24598535bb259d5e06c2f4d23067151e75ef961f
devonhackley <devonhackley@gmail.com>
2017-05-24 14:03:07 -0700

    worked through breadth first traverse

--- trees/trees.js
+++ trees/trees.js
@@ -11,6 +11,12 @@ const Tree = function(data){
   this._root = node;
 }
 
+Tree.prototype.appendChild = function(node){
+  if(!(node instanceof Node))
+    throw new Error('node must be type of Node')
+  this._root.children.push(node)
+}
+
 Tree.prototype.traveseDF = function(callback){
   (recurse(currentNode) => {
     for(var i=0, length = currentNode.children.length; i < length; i++){
@@ -20,16 +26,33 @@ Tree.prototype.traveseDF = function(callback){
   })(this._root);
 }
 
+Tree.prototype.prettyPrint = function(){
+  let result = '';
+
+  let queue = []
+
+  let breadthFirstTraverse = () => {
+    let next = queue.pop()
+    if(!next) return
+    result += next._root.data + ' '
+    next._root.children.forEach( child => queue.unshift(child))
+    breadthFirstTraverse()
+  }
+  breadthFirstTraverse()
+  console.log(result);
+  return result;  
+}
+
 
 var tree = new Tree('one');
 
-tree._root.children.push(new Node('two'));
+tree.appendChild(new Node('two'));
 tree._root.children[0].parent = tree;
 
-tree._root.children.push(new Node('three'));
+tree.appendChild(new Node('three'));
 tree._root.children[1].parent = tree;
 
-tree._root.children.push(new Node('four'));
+tree.appendChild(new Node('four'));
 tree._root.children[2].parent = tree;
 
 tree._root.children[0].children.push(new Node('five'));
