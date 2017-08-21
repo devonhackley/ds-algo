3030adbccde8aab1a99c0849406e6db8c7cbb74c
devonhackley <devonhackley@gmail.com>
2017-08-21 16:15:05 -0700

    fixed one thing

--- isUnique.js
+++ isUnique.js
@@ -17,20 +17,5 @@ function isUnqiue(str){
  return true;
 }
 
-var allUniqueChars = function(string) {
-
-  // O(n^2) approach, no additional data structures used
-  // for each character, check remaining characters for duplicates
-  for (var i = 0; i < string.length; i++) {
-    for (var j = i + 1; j < string.length; j++) {
-      if (string[i] === string[j]) {
-        return false; // if match, return false
-      }
-    }
-  }
-  return true; // if no match, return true
-};
-
 var test = '38456hgf8';
-console.log(allUniqueChars(test));
 console.log(isUnqiue(test));
