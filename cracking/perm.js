55f653a2d75b517367081b896af261d9b88ccf5b
devonhackley <devonhackley@gmail.com>
2017-08-21 17:09:16 -0700

    working thru problem

---
+++ cracking/perm.js
@@ -0,0 +1,19 @@
+'use strict';
+
+// Cracking the Coding Interview: Permutation
+
+function checkPerm(str1, str2){
+  let s1 = str1.toLowerCase();
+  let s2 = str2.toLowerCase();
+  let count = 0;
+
+  let strHash = {};
+  for(var char in s1){
+    
+  }
+
+}
+
+var test1 = "ab";
+var test2 = "eidbaooo";
+console.log(checkPerm(test1, test2));
