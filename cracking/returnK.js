a47e2d501044e356163748c29d288ee6dffab4ac
devonhackley <devonhackley@gmail.com>
2017-09-01 13:05:57 -0700

    this one is tough

--- cracking/returnK.js
+++ cracking/returnK.js
@@ -11,32 +11,47 @@ const LinkedList = function(){
 }
 
 function returnK(list, k){
-  let current = list.head;
-  let counter = 0;
 
-  if(!current) return 0;
+  let current = list.head; //first pointer
 
-  counter = (returnK(current.next, k)) + 1;
+  if(!current) return 0; //base case if head is null
 
-  if(counter === k){
-    console.log(k +'th to the last place is ' + counter);
+  //Moving first pointer by k amount
+  while(k > 0){
+    current = current.next;
+    k--;
   }
-  console.log(counter);
-  return counter;
+
+
+  let nextNode = head; //second pointer
+
+  // Move both pointers together, when the first is at the end, the second pointer will be the kth node
+  while(!current){
+    current = current.next;
+    nextNode = nextNode.next;
+  }
+
+  let value = nextNode.val;
+  return value;
 }
 
 //Test
 
 var temp = new LinkedList();
-var node1 = new Node(2);
-var node2 = new Node(3);
-var node3 = new Node(4);
-var node4 = new Node(6);
+var node1 = new Node(1);
+var node2 = new Node(2);
+var node3 = new Node(3);
+var node4 = new Node(4);
+var node5 = new Node(5);
+var node6 = new Node(6);
 
 
 temp.head = node1;
 node1.next = node2;
 node2.next = node3;
 node3.next = node4;
-
+node4.next = node5;
+node5.next = node6;
+// console.log(temp.head);
+// console.log(temp.head.next);
 console.log(returnK(temp,3));
