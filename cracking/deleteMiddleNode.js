92d474fe1090986a54c9279cc2f028522f6a9ac1
devonhackley <devonhackley@gmail.com>
2017-09-06 07:46:10 -0700

    delete node function working correctly

--- cracking/deleteMiddleNode.js
+++ cracking/deleteMiddleNode.js
@@ -14,18 +14,23 @@ const LinkedList = function(){
   this.head = null;
 }
 
-LinkedList.prototype.deleteNode = function(list, value){
+function deleteNode(list,value){
   let currentNode = list.head;
-
+    console.log('value', value);
   if(currentNode.val === value){
+      console.log('here first');
       return list.head.next;
   }
-  while(!currentNode.next){
+  while(currentNode.next !== null){
+    console.log('here second');
     if(currentNode.next.val === value){
+      console.log('here third');
+      currentNode.next = currentNode.next.next;
       return list.head;
     }
     currentNode = currentNode.next;
   }
+  console.log('new list',list);
   return list;
 }
 
@@ -35,7 +40,7 @@ function deleteMiddle(list){
   let nextNode = current.next;
 
   while(!current.next){
-    
+
 
   }
 };
@@ -46,18 +51,18 @@ function deleteMiddle(list){
 var beemp = new LinkedList();
 var node11 = new Node(1);
 var node12 = new Node(2);
-// var node13 = new Node(3);
-// var node14 = new Node(4);
-// var node15 = new Node(5);
-// var node16 = new Node(6);
-//
-//
-//
+var node13 = new Node(3);
+var node14 = new Node(4);
+var node15 = new Node(5);
+var node16 = new Node(6);
+
+
 beemp.head = node11;
 node11.next = node12;
-// node12.next = node13;
-// node13.next = node14;
-// node14.next = node15;
-// node15.next = node16;
+node12.next = node13;
+node13.next = node14;
+node14.next = node15;
+node15.next = node16;
 
+deleteNode(beemp, 3);
 console.log('dasdasdas',beemp.head);
