f1adf31ecfc5a426e431fee78be9c7e9afa7f87a
devonhackley <devonhackley@gmail.com>
2017-09-07 17:17:15 -0700

    starting new challenge

---
+++ cracking/intersection.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//Coding Challenge: Find the intersection of two lists of integers. Intersection is marked by a ';'. return the two integers in increasing order (i.e 6,3,5;4,7,8 == return 4,5)
+
+
+function intersection(list){
+  
+}
