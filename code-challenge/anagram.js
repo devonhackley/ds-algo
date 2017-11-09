c12051cb0eefef648a13446a7ec111b6cfa28e4b
devonhackley <devonhackley@gmail.com>
2017-11-08 16:54:09 -0800

    working thru anagram challenge

---
+++ code-challenge/anagram.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/* Find all possible anagrams of a word in a set of words */
+
+
+
+function findAnagram(word){
+  var anagrams = {}, results = {};
+
+  var wordsArr = words.map((word) => {
+    if(anagrams.hasOwnProperty(word))
+  })
+
+  return results;
+
+}
+
+
+var testWords = ['abcd', 'adcb', 'bdca', 'aaab'];
